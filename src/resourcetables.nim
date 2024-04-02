c306b0e2f4fa0c03563beffc1e00f1ef7354bcea
py-am-i <6770988+RattleyCooper@users.noreply.github.com>
2024-04-02 14:18:10 -0700

    Update resourcetables.nim

--- src/resourcetables.nim
+++ src/resourcetables.nim
@@ -7,8 +7,78 @@ import std/[os, macros, tables, strutils]
 type 
   ResourceTable* = Table[string, string]
 
+macro embed*(tableName: static[string], x: untyped): untyped =
+  ## Create a block of filepaths and the files will get embedded 
+  ## into the executable in a table that's identified by the name 
+  ## that was passed to `embed`. Use triple quoted strings to
+  ## retain the full path in the resulting ResourceTable.
+  ## 
+  ## Example:
+  ## 
+  ##    embed("data"):
+  ##      "C:/somefile.txt"
+  ##      """C:/otherfile.txt"""
+  ##    
+  ##    discard data["somefile.txt"]
+  ##    discard data["C:/otherfile.txt"]
+  ## 
+
+  result = newStmtList()
+  var tableDef = newStmtList()
+  let rdent = ident("r")
+  let ndent = ident("name")
+  tableDef.add quote do:
+    var `rdent`: ResourceTable
+
+  # Add static reads to table
+  for l in x:
+    if l.kind == nnkTripleStrLit:   
+      tableDef.add quote do:
+        `rdent`[`l`] = staticRead(`l`)
+    elif l.kind == nnkStrLit:
+      tableDef.add quote do:
+        var n = block:
+          if `l`.contains("/"):
+            `l`.split("/")[^1]
+          elif `l`.contains("\\"):
+            `l`.split("\\")[^1]
+          else:
+            `l`
+        `rdent`[n] = staticRead(`l`)
+  tableDef.add quote do:
+    `rdent`
+
+  let blockStmt = nnkBlockStmt.newTree(
+    newEmptyNode(), tableDef
+  )
+  let constDef = nnkConstDef.newTree(
+    ident(tableName), newEmptyNode(), blockStmt
+  )
+  result.add nnkConstSection.newTree(constDef)
+  when defined(debug):
+    echo result.repr
+
+proc embed*(directory: string): ResourceTable =
+  ## Embed an entire directory into a ResourceTable.
+  ## 
+  ## example:
+  ##   const assets: ResourceTable = embed("assets")
+  ##   echo assets["assets/someImg.png"]
+  #
+  var pages: ResourceTable
+  for fd in walkDir(directory):
+    if fd.kind == pcFile:
+      var p = fd.path.replace("\\", "/")
+      pages[p] = staticRead(p)
+  when defined(debug):
+    echo pages.repr      
+  pages
+
+
 macro resources*(u: untyped): untyped =
-  ## Embed resourcs into an executable and make them available in a 
+  ## Deprecated. Use embed(tableName):
+  ## 
+  ## Embed resources into an executable and make them available in a 
   ## ResourceTable, which is just a Table[string, string]
   ## 
   ## example:
@@ -34,18 +104,6 @@ macro resources*(u: untyped): untyped =
       `pident`[`l`] = staticRead(`l`)
     examine.add(line)
   examine.add(quote do: `pident`)
-  result = examine
-
-proc embed*(directory: string): ResourceTable =
-  ## Embed an entire directory into a ResourceTable.
-  ## 
-  ## example:
-  ##   const assets: ResourceTable = embed("assets")
-  ##   echo assets["assets/someImg.png"]
-  #
-  var pages: ResourceTable
-  for fd in walkDir(directory):
-    if fd.kind == pcFile:
-      var p = fd.path.replace("\\", "/")
-      pages[p] = staticRead(p)
-  pages
+  when defined(debug):
+    echo examine.repr
+  examine
